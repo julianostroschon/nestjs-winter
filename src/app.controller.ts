9fe551d734d01c3204a4b92c44bac635cfbc8e88
julianostroschon <julianostroschon@gmail.com>
2024-05-06 16:11:58 -0300

    ✨ Add acesso à request

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,15 @@
-import { Controller, Post } from '@nestjs/common';
+import { Controller, Get, Req } from '@nestjs/common';
 import { InjectKnex, Knex } from 'nestjs-knex';
 
 @Controller()
 export class AppController {
   constructor(@InjectKnex() private readonly db: Knex) {}
-  @Post()
-  async getHello(): Promise<{ message: string }> {
+  @Get()
+  async getHello(@Req() request: Request): Promise<{ message: string }> {
     const user = await this.db<{ nome: string }>('usuarios')
       .where('email', 'demo@demo.com')
       .first('nome');
+    console.log({ request });
     return { message: `Hello World, ${user.nome}` };
   }
 }
