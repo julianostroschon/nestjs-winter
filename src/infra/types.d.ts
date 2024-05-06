e856dee595a51501e1902b8419a6c232303de8ea
julianostroschon <julianostroschon@gmail.com>
2024-05-06 16:03:36 -0300

    ✏️ Cria DBConfig

---
+++ src/infra/types.d.ts
@@ -0,0 +1,9 @@
+export interface DBConfig {
+  DB_SSL_CERTIFICATE: boolean;
+  DB_USERNAME: string;
+  DB_PASSWORD: string;
+  DB_DRIVER: string;
+  DB_HOST: string;
+  DB_PORT: number;
+  DB_NAME: string;
+}
