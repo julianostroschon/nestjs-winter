697a96d08c09023ff8ea6b2a69acdca34e09edda
julianostroschon <julianostroschon@gmail.com>
2024-05-14 07:53:25 -0300

    ➖ Remove zod from infra/env

--- src/infra/env.ts
+++ src/infra/env.ts
@@ -1,27 +1,28 @@
 import { config } from 'dotenv';
-import { z } from 'zod';
+// import { z } from 'zod';
 import { DBConfig } from './types';
 
 config();
 
-const envRaw = z.object({
-  DB_SSL_CERTIFICATE: z.boolean().default(false),
-  DB_PASSWORD: z.string().default('53cr3T'),
-  DB_USERNAME: z.string().default('root'),
-  DB_NAME: z.string().default('graphql'),
-  DB_DRIVER: z.string().default('pg'),
-  DB_PORT: z.coerce.number().int().gte(100).lte(64999).default(5432),
-  DB_HOST: z.string().default('localhost'),
-});
+// const envRaw = z.object({
+//   DB_SSL_CERTIFICATE: z.boolean().default(false),
+//   DB_PASSWORD: z.string().default('53cr3T'),
+//   DB_USERNAME: z.string().default('root'),
+//   DB_NAME: z.string().default('graphql'),
+//   DB_DRIVER: z.string().default('pg'),
+//   DB_PORT: z.coerce.number().int().gte(100).lte(64999).default(5432),
+//   DB_HOST: z.string().default('localhost'),
+// });
 
-export const env = envRaw.parse(process.env);
+export const env = process.env;
+// export const env = envRaw.parse(process.env);
 
 export const databaseConfig: DBConfig = Object.freeze({
-  DB_SSL_CERTIFICATE: env.DB_SSL_CERTIFICATE,
+  DB_SSL_CERTIFICATE: !!env.DB_SSL_CERTIFICATE,
   DB_USERNAME: env.DB_USERNAME,
   DB_PASSWORD: env.DB_PASSWORD,
   DB_DRIVER: env.DB_DRIVER,
-  DB_PORT: env.DB_PORT,
+  DB_PORT: Number(env.DB_PORT),
   DB_HOST: env.DB_HOST,
   DB_NAME: env.DB_NAME,
 });
